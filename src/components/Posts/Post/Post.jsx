e887d2707c366a2a003741a9dd0cab15eb8adfa3
Nikkakko <nikolozkopadze@gmail.com>
2022-12-06 18:25:17 +0400

    fixed media

--- src/components/Posts/Post/Post.jsx
+++ src/components/Posts/Post/Post.jsx
@@ -108,9 +108,6 @@ const StyledMedia = styled(CardMedia)`
   padding-top: 56.25%;
   background-color: rgba(0, 0, 0, 0.5);
   background-blend-mode: darken;
-
-  @media (max-width: 375px) {
-  }
 `;
 
 const StyledCard = styled(Card)`
