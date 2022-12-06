102bf9473894dc8664da102664d4f0585694cd9f
Nikkakko <nikolozkopadze@gmail.com>
2022-12-06 14:02:24 +0400

    fixed

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    assetsInlineLimit: '2048', // 2kb
+  },
 });
