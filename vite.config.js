8babc5a491a8cfb706f05bd368eddb4df7758220
Nikkakko <nikolozkopadze@gmail.com>
2022-12-06 10:02:30 +0400

    fixed

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  chunkSizeWarningLimit: 500,
+  plugins: [react(), splitVendorChunkPlugin()],
 });
